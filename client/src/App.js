a9aee6caa32f2e4d14222ef9e9479d138ceddb4d
Dennis Peter <peterdennis573@gmail.com>
2024-08-20 07:52:32 +0300

    Updates on the client side folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import Home from "./pages/Customers/Home/Home";
+
+import Cart from "./pages/Customers/Cart/Cart";
+
+import Checkout from "./pages/Customers/Checkout/Checkout";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/customer/checkout" element={<Checkout />} />
+        <Route path="/customer/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
