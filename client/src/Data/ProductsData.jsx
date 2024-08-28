3e5e8233000aa7ba8f2b821b7a83e9cf0ee10daf
Dennis Peter <peterdennis573@gmail.com>
2024-08-28 08:51:26 +0300

    Updates{

---
+++ client/src/Data/ProductsData.jsx
@@ -0,0 +1,37 @@
+import TestimonialImage1  from "../assets/images/assets/menu_1.png"
+import TestimonialImage2  from "../assets/images/assets/menu_2.png"
+import TestimonialImage3  from "../assets/images/assets/menu_3.png"
+import TestimonialImage4  from "../assets/images/assets/menu_4.png"
+const testimonials = [
+    {
+      name: "Ken Anelka",
+      role: "CEO Supa Loaf",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ullam beatae sit ut dolore quam? Vitae beatae maxime sint qui. Tenetur molestias consectetur quas laudantium velit similique ut.",
+      image:TestimonialImage1,
+    },
+    {
+      name: "Ken Anelka",
+      role: "CEO Supa Loaf",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ullam beatae sit ut dolore quam? Vitae beatae maxime sint qui. Tenetur molestias consectetur quas laudantium velit similique ut.",
+      image:TestimonialImage2,
+    },
+    {
+      name: "Ken Anelka",
+      role: "CEO Supa Loaf",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ullam beatae sit ut dolore quam? Vitae beatae maxime sint qui. Tenetur molestias consectetur quas laudantium velit similique ut.",
+      image:TestimonialImage3,
+    },
+    {
+      name: "Ken Anelka",
+      role: "CEO Supa Loaf",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ullam beatae sit ut dolore quam? Vitae beatae maxime sint qui. Tenetur molestias consectetur quas laudantium velit similique ut.",
+      image:TestimonialImage4,
+    },
+    // Add more testimonials here
+  ];
+
+  export {testimonials};
--- client/src/pages/Customers/Home/Testimonials.jsx
+++ client/src/pages/Customers/Home/Testimonials.jsx
@@ -1,30 +1,36 @@
 import React from "react";
-import "./Testimonials.css";
 import Slider from "react-slick";
+import "./Testimonials.css";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import TestimonialImage from "../../../assets/images/assets/menu_1.png";
+import { testimonials } from "../../../Data/ProductsData";
 
 const Testimonials = () => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
-
-  const testimonials = [
-    {
-      name: "Ken Anelka",
-      role: "CEO Supa Loaf",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ullam beatae sit ut dolore quam? Vitae beatae maxime sint qui. Tenetur molestias consectetur quas laudantium velit similique ut.",
-      image: TestimonialImage,
-    },
-    // Add more testimonials here
-  ];
 
   return (
     <div className="testimonials">
@@ -41,7 +47,7 @@ const Testimonials = () => {
                   </div>
                   <div className="row">
                     <p className="name">{testimonial.name}</p>
-                    <p>{testimonial.role}</p>
+                    <p className="role">{testimonial.role}</p>
                   </div>
                 </div>
                 <div className="testimonial-body">
