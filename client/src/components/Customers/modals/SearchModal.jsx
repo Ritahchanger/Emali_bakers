17b4088e39725c993f0988a4249c014e0bb01df2
Dennis Peter <peterdennis573@gmail.com>
2024-09-08 07:38:02 +0300

    Updates

--- client/src/components/Customers/modals/SearchModal.jsx
+++ client/src/components/Customers/modals/SearchModal.jsx
@@ -6,6 +6,8 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { closeSearchModal } from "../../../Redux/Features/SearchSlice";
 
+import ModalTestingImage from "../../../assets/images/assets/food_11.png";
+
 const SearchModal = () => {
   const dispatch = useDispatch();
 
@@ -19,7 +21,7 @@ const SearchModal = () => {
 
   return (
     <div
-      className={`modal search-modal ${displaySearchModal ? "active" : null}`}
+      className={`modal search-modal ${displaySearchModal ? "active" : null} `}
     >
       <div className="modal-wrapper">
         <button className="close-button" onClick={handleCloseModal}>
@@ -27,6 +29,50 @@ const SearchModal = () => {
         </button>
         <div className="modal-contents-container">
           <p className="small-header">{"SEARCH RESULTS"}</p>
+          <div className="grid">
+            <div className="card">
+              <div className="img-wrapper">
+                <img src={ModalTestingImage} alt="" />
+                <p>Lorem, ipsum dolor.</p>
+                <p>Lorem, ipsum dolor.</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img-wrapper">
+                <img src={ModalTestingImage} alt="" />
+                <p>Lorem, ipsum dolor.</p>
+                <p>Lorem, ipsum dolor.</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img-wrapper">
+                <img src={ModalTestingImage} alt="" />
+                <p>Lorem, ipsum dolor.</p>
+                <p>Lorem, ipsum dolor.</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img-wrapper">
+                <img src={ModalTestingImage} alt="" />
+                <p>Lorem, ipsum dolor.</p>
+                <p>Lorem, ipsum dolor.</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img-wrapper">
+                <img src={ModalTestingImage} alt="" />
+                <p>Lorem, ipsum dolor.</p>
+                <p>Lorem, ipsum dolor.</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img-wrapper">
+                <img src={ModalTestingImage} alt="" />
+                <p>Lorem, ipsum dolor.</p>
+                <p>Lorem, ipsum dolor.</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
