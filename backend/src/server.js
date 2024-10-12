84a5f01901755b968ee47048962c3097fd3b8a34
Dennis Peter <peterdennis573@gmail.com>
2024-10-12 09:37:13 +0300

    Started a new server

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+const PORT = process.env.PORT || 8000
+
+const app = require("./app");
+
+
+app.listen(PORT,()=>{
+
+    console.log(`There server is successfully running on PORT ${PORT}`)
+
+})
--- server/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (err,req,res,next)=>{
-
-    return res.status(500).json({message:err});
-
-}
-
-
-module.exports = errorHandler;
