e7e002f038c0b8f2554f7ec9491ef703dc0bd3c7
Dennis Peter <peterdennis573@gmail.com>
2024-10-14 12:18:25 +0300

    Updates on a controller to update item quantity

--- backend/controllers/CartController.js
+++ backend/controllers/CartController.js
@@ -25,6 +25,83 @@ const getCartItems = async (req,res,next)=>{
 
 }
 
+const addItemsToCart = async (req,res,next)=>{
+
+    try{
+
+        const { userId,productId,quantity} = req.body;
+
+        const product = await Product.findById(productId);
+
+        if(!product){
+
+            return res.status(404).json({message:'Product not found'})
+
+        }
+
+        let cart = await Cart.findOne({user:userId});
+
+        if(!cart){
+
+            cart = new Cart({user:userId,items:[]});
+
+        }
+
+        const existingItem = cart.items.find(item => item.product.toString()===productId);
+
+        if(existingItem){
+
+            existingItem.quantity += quantity
+        }
+
+        cart.items.push({product:productId,quantity});
+
+        await cart.save()
+
+        res.status(201).json({ success:true,data:cart })
+    
+    }catch(error){
+        next(error)
+    }
+
+}
+
+const updateItemQuantity = async (req,res,next) => {
+
+    const { userId,productId,quantity } = req.body;
+
+    try{
+
+        const cart = await Cart.findOne({user:userId});
+
+        if(!cart){
+
+            return res.status(200).json({status:404,success:false,message:'Cart not found'});
+
+        }
+
+        const item = cart.items.find(item=>item.product.toString()===productId);
+
+        if(!item){
+
+            return res.status(200).json({status:404,success:false,message:'Items not found in cart'})
+        }
+
+        item.quantity = quantity;
+
+        await cart.save();
+
+        res.status(200).json({status:200,success:true,data:Cart})
+
+    }catch(error){
+
+        next(error)
+
+    }
 
 
-module.exports = { getCartItems }
+}
+
+
+
+module.exports = { getCartItems,updateItemQuantity }
