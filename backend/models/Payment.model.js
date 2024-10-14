f434dcf2faebd70db256bc88151b81b66795e963
Dennis Peter <peterdennis573@gmail.com>
2024-10-14 12:49:06 +0300

    Worked on a model to store payment details

---
+++ backend/models/Payment.model.js
@@ -0,0 +1,98 @@
+const mongoose= require("mongoose");
+
+
+const PaymentSchema = new mongoose.Schema({
+
+    user:{
+
+        type:mongoose.Schema.Types.ObjectId,
+
+        ref:"Business",
+
+        required:true
+    },
+
+    amount:{
+
+        type:Number,
+        required:true,
+
+        min:0,
+
+    },
+
+    paymentMethod:{
+
+        type:String,
+
+        required:true,
+
+        enum:["daraja","stripe"],
+
+    },
+
+    transactionId:{
+
+        type:String,
+
+        required:true,
+    },
+
+    status:{
+
+        type:String,
+
+        enum:["pending","completed"],
+
+        default:"pending"
+
+    },
+    createdAt:{
+
+        type:Date,
+
+        default:Date.now,
+
+    },
+
+    updatedAt:{
+
+        type:Date,
+
+
+        default:Date.now
+    },
+
+    darajaDetails:{
+        transactionReference:{
+            type:String,
+
+            required:function(){
+                return this.paymentMethod ==="daraja"
+            }
+        }
+    },
+
+    strifeDetails:{
+
+        chargeId:{
+
+            type:String,
+
+            required:function(){
+
+                return this.paymentMethod === "stripe"
+            }
+
+        }
+
+    }
+
+
+},{ timestamps:true })
+
+
+const Payment = mongoose.model("Payment",PaymentSchema)
+
+
+module.exports = Payment;
