30ce19427555df8988ef7da9ea9bb72f525e0da8
Dennis Peter <peterdennis573@gmail.com>
2024-10-16 15:40:42 +0300

    Updates on the admin pages

--- client/src/pages/Admin/AdminUsers/AdminUsers.jsx
+++ client/src/pages/Admin/AdminUsers/AdminUsers.jsx
@@ -103,6 +103,70 @@ const AdminUsers = () => {
       },
       bussUsername: "bakerytwenty",
     },
+    {
+      bssName: "Bakery Twenty",
+      bssType: "Pastry",
+      bssPhone: "0712987654",
+      bssEmail: "bakery20@example.com",
+      countyName: "Kisumu",
+      cityName: "Kisumu",
+      streetName: "Oginga Odinga Street",
+      contactPerson: {
+        fName: "Jane",
+        lName: "Smith",
+        jobTitle: "Owner",
+        phoneNo: "0712987654",
+      },
+      bussUsername: "bakerytwenty",
+    },
+    {
+      bssName: "Bakery Twenty",
+      bssType: "Pastry",
+      bssPhone: "0712987654",
+      bssEmail: "bakery20@example.com",
+      countyName: "Kisumu",
+      cityName: "Kisumu",
+      streetName: "Oginga Odinga Street",
+      contactPerson: {
+        fName: "Jane",
+        lName: "Smith",
+        jobTitle: "Owner",
+        phoneNo: "0712987654",
+      },
+      bussUsername: "bakerytwenty",
+    },
+    {
+      bssName: "Bakery Twenty",
+      bssType: "Pastry",
+      bssPhone: "0712987654",
+      bssEmail: "bakery20@example.com",
+      countyName: "Kisumu",
+      cityName: "Kisumu",
+      streetName: "Oginga Odinga Street",
+      contactPerson: {
+        fName: "Jane",
+        lName: "Smith",
+        jobTitle: "Owner",
+        phoneNo: "0712987654",
+      },
+      bussUsername: "bakerytwenty",
+    },
+    {
+      bssName: "Bakery Twenty",
+      bssType: "Pastry",
+      bssPhone: "0712987654",
+      bssEmail: "bakery20@example.com",
+      countyName: "Kisumu",
+      cityName: "Kisumu",
+      streetName: "Oginga Odinga Street",
+      contactPerson: {
+        fName: "Jane",
+        lName: "Smith",
+        jobTitle: "Owner",
+        phoneNo: "0712987654",
+      },
+      bussUsername: "bakerytwenty",
+    },
   ];
 
   return (
@@ -111,69 +175,79 @@ const AdminUsers = () => {
       <AdminSidebar />
       <div className="container">
         <div className="users-container">
-          <table>
-            <thead>
-              <tr>
-                <th>BSS NAME</th>
-                <th>BSS TYPE</th>
-                <th>BSS PHONE</th>
-                <th>BSS EMAIL</th>
-                <th>COUNTY NAME</th>
-                <th>CITY NAME</th>
-                <th>STREET NAME</th>
-                <th>CONTACT PERSON</th>
-                <th>BUSS USERNAME</th>
-              </tr>
-              <tr>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th>
-                  <table>
-                    <thead>
-                      <tr>
-                        <th>FNAME</th>
-                        <th>LNAME</th>
-                        <th>JOB TITLE</th>
-                        <th>PHONE NO</th>
-                      </tr>
-                    </thead>
-                  </table>
-                </th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {dummyData.map((data, index) => (
-                <tr key={index}>
-                  <td>{data.bssName}</td>
-                  <td>{data.bssType}</td>
-                  <td>{data.bssPhone}</td>
-                  <td>{data.bssEmail}</td>
-                  <td>{data.countyName}</td>
-                  <td>{data.cityName}</td>
-                  <td>{data.streetName}</td>
-                  <td>
-                    <table>
-                      <tbody>
-                        <tr>
-                          <td>{data.contactPerson.fName}</td>
-                          <td>{data.contactPerson.lName}</td>
-                          <td>{data.contactPerson.jobTitle}</td>
-                          <td>{data.contactPerson.phoneNo}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </td>
-                  <td>{data.bussUsername}</td>
+          <div className="table-wrapper">
+            <table>
+              <thead
+                style={{
+                  background: "#340290",
+                }}
+              >
+                <tr>
+                  <th>BSS NAME</th>
+                  <th>BSS TYPE</th>
+                  <th>BSS PHONE</th>
+                  <th>BSS EMAIL</th>
+                  <th>COUNTY NAME</th>
+                  <th>CITY NAME</th>
+                  <th>STREET NAME</th>
+                  <th>CONTACT PERSON</th>
+                  <th>BUSS USERNAME</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+                <tr>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th>
+                    <table>
+                      <thead
+                        style={{
+                          background: "var(--header3)",
+                        }}
+                      >
+                        <tr>
+                          <th>FNAME</th>
+                          <th>LNAME</th>
+                          <th>JOB TITLE</th>
+                          <th>PHONE NO</th>
+                        </tr>
+                      </thead>
+                    </table>
+                  </th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {dummyData.map((data, index) => (
+                  <tr key={index}>
+                    <td>{data.bssName}</td>
+                    <td>{data.bssType}</td>
+                    <td>{data.bssPhone}</td>
+                    <td>{data.bssEmail}</td>
+                    <td>{data.countyName}</td>
+                    <td>{data.cityName}</td>
+                    <td>{data.streetName}</td>
+                    <td>
+                      <table>
+                        <tbody>
+                          <tr>
+                            <td>{data.contactPerson.fName}</td>
+                            <td>{data.contactPerson.lName}</td>
+                            <td>{data.contactPerson.jobTitle}</td>
+                            <td>{data.contactPerson.phoneNo}</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                    <td>{data.bussUsername}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
