f23a92db90b1549f759270f05c87ac5f315b7f78
Dennis Peter <peterdennis573@gmail.com>
2024-12-08 16:53:44 +0300

    Updates

--- backend/middlewares/AuthenticateToken.js
+++ backend/middlewares/AuthenticateToken.js
@@ -3,38 +3,36 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 
-
 const verifyToken = (req,res,next) =>{
 
-   const token = req.headers['authorization']?.split('  ')[1] || req.cookies.token;
+    const token = req.headers['authorization']?.split(' ')[1]
 
-   if(!token){
+    if(!token){
 
-    return res.sendStatus(401)
-
-   }
-
-   jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
-
-    if(err){
-
-        return res.sendStatus(403);
+        return res.sendStatus(401);
 
     }
 
-    req.businessData =  {
 
-        id:decoded.id,
+    jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
 
-        username:decoded.username,
+        if(err){
 
-    };
+            return res.sendStatus(403);
 
-    next()
+        }
 
-   })
-   
+        req.user = {
 
+            id:decoded.id,
+
+            username:decoded.username,
+
+        }
+
+        next()
+
+    })
 }
 
 
